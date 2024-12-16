be039f869dcc6e9883d28328b265a0f17016bd00
DmytroLysachenko <dlysachenko98@gmail.com>
2024-12-16 22:44:49 +0100

    Added player

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -12,12 +12,14 @@ import images from "@/constants/images";
 import SearchInput from "@/components/SearchInput";
 import Trending from "@/components/Trending";
 import EmptyState from "@/components/EmptyState";
-import { getAllPosts } from "@/lib/appwrite";
+import { getAllPosts, getLatestPosts } from "@/lib/appwrite";
 import { useAppwrite } from "@/lib/useAppwrite";
 import VideoCard from "@/components/VideoCard";
 
 const Home = () => {
   const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
+
   const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = async () => {
@@ -61,7 +63,7 @@ const Home = () => {
               <Text className="text-gray-100 text-lg font-pregular mb-3">
                 Latest Videos
               </Text>
-              <Trending posts={posts ?? []} />
+              <Trending posts={latestPosts ?? []} />
             </View>
           </View>
         )}
--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,15 +1,102 @@
-import { View, Text, FlatList } from "react-native";
-import React from "react";
+import {
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+import { useVideoPlayer, VideoView } from "expo-video";
+import React, { useState } from "react";
+import * as Animatable from "react-native-animatable";
+import icons from "@/constants/icons";
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1,
+  },
+};
+
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+  const player = useVideoPlayer(item.video);
+  return (
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {play ? (
+        <VideoView
+          player={player}
+          style={{
+            width: 208,
+            height: 288,
+            borderRadius: 33,
+            backgroundColor: "rgb(255 255 255 / 0.1)",
+          }}
+          className="w-52 h-72 rounded-[33px] mt-3 bg-white/10"
+          nativeControls={false}
+        />
+      ) : (
+        <TouchableOpacity
+          className="relative justify-center items-center"
+          activeOpacity={0.7}
+          onPress={() => {
+            setPlay(true);
+            player.play();
+          }}
+        >
+          <ImageBackground
+            source={{ uri: item.thambnail }}
+            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            imageStyle={{ borderRadius: 10 }}
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </Animatable.View>
+  );
+};
 
 const Trending = ({ posts }: any) => {
+  const [activeItem, setActiveItem] = useState(posts[1]);
+
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
+
   return (
     <FlatList
       data={posts}
-      keyExtractor={(item) => item.id}
+      keyExtractor={(item) => item.$id}
       renderItem={({ item }) => (
-        <Text className="text-white text-3xl ">{item.id}</Text>
+        <TrendingItem
+          activeItem={activeItem}
+          item={item}
+        />
       )}
       horizontal
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{ itemVisiblePercentThreshold: 70 }}
+      contentOffset={{ x: 170, y: 0 }}
     ></FlatList>
   );
 };
